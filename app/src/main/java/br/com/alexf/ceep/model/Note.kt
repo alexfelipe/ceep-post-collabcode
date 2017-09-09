5ce68dccaf7657fc9d10159aaaa18f45f587410f
alexfelipe <alexfelipevieira@gmail.com>
2017-09-09 19:26:19 -0300

    implementando o recyclerview

---
+++ app/src/main/java/br/com/alexf/ceep/model/Note.kt
@@ -0,0 +1,4 @@
+package br.com.alexf.ceep.model
+
+class Note(val title: String,
+           val description: String)
--- app/src/main/java/br/com/alexf/ceep/ui/activity/NoteListActivity.kt
+++ app/src/main/java/br/com/alexf/ceep/ui/activity/NoteListActivity.kt
@@ -2,7 +2,11 @@ package br.com.alexf.ceep.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 import br.com.alexf.ceep.R
+import br.com.alexf.ceep.model.Note
+import br.com.alexf.ceep.ui.adapter.NoteListAdapter
 import kotlinx.android.synthetic.main.activity_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
@@ -10,7 +14,22 @@ class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
-        note_list_textview.text = "Bem vindo ao Ceep"
+
+        val recyclerView = note_list_recyclerview
+        recyclerView.adapter = NoteListAdapter(notes(), this)
+        val layoutManager = StaggeredGridLayoutManager(
+                2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layoutManager
+    }
+
+    fun notes(): List<Note> {
+        return listOf(
+                Note("Leitura",
+                        "Livro de Kotlin com Android"),
+                Note("Pesquisa",
+                        "Como posso melhorar o código dos meus projetos"),
+                Note("Estudo",
+                        "Como sincronizar minha App com um Web Service"))
     }
 
 }
