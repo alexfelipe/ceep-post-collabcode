4d2558b54d814257e41072fb62777fa4fbc04326
alexfelipe <alexfelipevieira@gmail.com>
2017-09-09 19:26:19 -0300

    implementando o recyclerview

---
+++ app/src/main/java/br/com/alexf/ceep/ui/adapter/NoteListAdapter.kt
@@ -0,0 +1,44 @@
+package br.com.alexf.ceep.ui.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.alexf.ceep.R
+import br.com.alexf.ceep.model.Note
+import kotlinx.android.synthetic.main.note_item.view.*
+
+class NoteListAdapter(private val notes: List<Note>,
+                      private val context: Context) : Adapter<NoteListAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        val note = notes[position]
+        holder?.let {
+            it.bindView(note)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.note_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(note: Note) {
+            val title = itemView.note_item_title
+            val description = itemView.note_item_description
+
+            title.text = note.title
+            description.text = note.description
+        }
+
+    }
+
+}
